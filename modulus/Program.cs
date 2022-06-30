59e7db26f2e6cfaa050b6ac92d139b2d633bd5ed
lebo.mofokeng <lebo.mofokeng@DDNAD4R7DB22>
2022-06-30 12:36:39 +0200

    Add project files.

---
+++ modulus/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace modulus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount , saveAmnt;
+            int twenties, tens, fives, ones;
+
+            Write("Please enter the amount : ");
+            amount =Convert.ToInt32(ReadLine());
+
+            saveAmnt = amount;
+
+            twenties = amount / 20;
+            amount = amount % 20;
+            tens = amount / 10;
+            amount = amount % 10;
+            fives = amount / 5;
+            ones = amount % 5;
+
+            //WriteLine(saveAmnt + " amount");
+            WriteLine(twenties + " Twenties\n" + tens + " Tens\n" + fives + " Fives\n" + ones + " Ones");
+
+        
+        }
+    }
+}
